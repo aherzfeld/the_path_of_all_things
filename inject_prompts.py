893ce10059863ee7567ee8cecccf2bbd981ad438
Andrew <herzfeld.andrew@gmail.com>
2026-02-24 13:31:22 +0900

    corrected filename in script

--- inject_prompts.py
+++ inject_prompts.py
@@ -1,24 +1,21 @@
 import json
 
-# Load your original file
-with open('data.json', 'r', encoding='utf-8') as f:
+# 1. Update this line to look in the src folder
+with open('src/data.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-# The template you know works
 PROMPT_TEMPLATE = (
     "Can you generate an image for \"{title}\" in the following style: "
     "Ink Wash & Sumi-e (True Wabi-Sabi). For a more poetic feel, use "
     "abstract ink-bleed or wash illustrations."
 )
 
-# Update every event in every level
 for level in data['levels']:
     for event in level['events']:
-        # Adding the new key 'image_prompt'
         event['image_prompt'] = PROMPT_TEMPLATE.format(title=event['title'])
 
-# Save the updated file
-with open('data_with_prompts.json', 'w', encoding='utf-8') as f:
+# 2. Update this line to save it back into the src folder
+with open('src/data_with_prompts.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, indent=2)
 
-print("Done! Your updated file is saved as 'data_with_prompts.json'.")
+print("Done! Check your 'src' folder for 'data_with_prompts.json'.")
